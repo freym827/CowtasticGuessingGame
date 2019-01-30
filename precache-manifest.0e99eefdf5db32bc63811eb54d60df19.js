752f83228d89cb0f09ceaedcf490bdd607eeea0b
Bitter Starglow <freym827@gmail.com>
2019-01-29 19:55:45 -0500

    Updates

---
+++ precache-manifest.0e99eefdf5db32bc63811eb54d60df19.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b2d7107479cccf48931c044b0cc1b27",
+    "url": "/CowtasticGuessingGame/static/media/hereford.9b2d7107.jpg"
+  },
+  {
+    "revision": "01b858d018f021575451",
+    "url": "/CowtasticGuessingGame/static/css/main.2940b809.chunk.css"
+  },
+  {
+    "revision": "e30957391f541d6e3f0e",
+    "url": "/CowtasticGuessingGame/static/js/1.e3095739.chunk.js"
+  },
+  {
+    "revision": "20b6f71c63e46b5d892d",
+    "url": "/CowtasticGuessingGame/static/js/runtime~main.20b6f71c.js"
+  },
+  {
+    "revision": "9e14c974171c06ac1ec4161e6b5b4e2b",
+    "url": "/CowtasticGuessingGame/static/media/angus.9e14c974.jpg"
+  },
+  {
+    "revision": "1c7bbc53cb04c742ab1cee15b60ef653",
+    "url": "/CowtasticGuessingGame/static/media/holstein.1c7bbc53.jpg"
+  },
+  {
+    "revision": "7e9b352a0d05520e6d557f7bed25bd6d",
+    "url": "/CowtasticGuessingGame/static/media/wagyu.7e9b352a.jpg"
+  },
+  {
+    "revision": "004a66fd35057fc2e1870a6b3c413ef4",
+    "url": "/CowtasticGuessingGame/static/media/beefalo.004a66fd.jpg"
+  },
+  {
+    "revision": "1bffb24131d7354ba9842c27ab3d03f6",
+    "url": "/CowtasticGuessingGame/static/media/galloway.1bffb241.jpg"
+  },
+  {
+    "revision": "2a192e749965f7d126bb9fecac386da2",
+    "url": "/CowtasticGuessingGame/static/media/brahman.2a192e74.jpg"
+  },
+  {
+    "revision": "84301b3e8ecf94d04b1e01863eff7928",
+    "url": "/CowtasticGuessingGame/static/media/charolais.84301b3e.jpg"
+  },
+  {
+    "revision": "2ebdddb8a94abc05200d4218120d9ad5",
+    "url": "/CowtasticGuessingGame/static/media/dexter.2ebdddb8.jpg"
+  },
+  {
+    "revision": "f89b30cc5e884bd670c0a950ff2dec65",
+    "url": "/CowtasticGuessingGame/static/media/gelbvieh.f89b30cc.jpg"
+  },
+  {
+    "revision": "01b858d018f021575451",
+    "url": "/CowtasticGuessingGame/static/js/main.01b858d0.chunk.js"
+  },
+  {
+    "revision": "e2b5b579492be913ebfbee964f765272",
+    "url": "/CowtasticGuessingGame/static/media/highland.e2b5b579.jpg"
+  },
+  {
+    "revision": "13e8a38a65daed53e9709dd61429152a",
+    "url": "/CowtasticGuessingGame/static/media/watusi.13e8a38a.jpg"
+  },
+  {
+    "revision": "1a4e1305b229f4069d76ed5a6b2dc5bd",
+    "url": "/CowtasticGuessingGame/static/media/ayrshire.1a4e1305.jpg"
+  },
+  {
+    "revision": "1913135247f7898187fb1db3d5cf07c1",
+    "url": "/CowtasticGuessingGame/static/media/piedmontese.19131352.png"
+  },
+  {
+    "revision": "7d4909b7295e3a13229ec02db900f900",
+    "url": "/CowtasticGuessingGame/static/media/sindhi.7d4909b7.jpg"
+  },
+  {
+    "revision": "4a897259fae9af5bd6db73464c69c621",
+    "url": "/CowtasticGuessingGame/static/media/elsa.4a897259.jpg"
+  },
+  {
+    "revision": "8d8b82c1682abbf7d4cb843a60cc1111",
+    "url": "/CowtasticGuessingGame/static/media/singlecow.8d8b82c1.mp3"
+  },
+  {
+    "revision": "dbd0e69e0c5fedd37bea875e0aa75332",
+    "url": "/CowtasticGuessingGame/static/media/rooster.dbd0e69e.mp3"
+  },
+  {
+    "revision": "baad2ca967e715ae6f4e9207471343f3",
+    "url": "/CowtasticGuessingGame/static/media/sadtrombone.baad2ca9.mp3"
+  },
+  {
+    "revision": "408211d9b8396714e3c027eaa43ba4fa",
+    "url": "/CowtasticGuessingGame/static/media/cowherd.408211d9.mp3"
+  },
+  {
+    "revision": "c2030d5be0b9612b93380594a7d873cd",
+    "url": "/CowtasticGuessingGame/static/media/applause.c2030d5b.mp3"
+  },
+  {
+    "revision": "59e50007f3d4605c9dbe8e255dbfd000",
+    "url": "/CowtasticGuessingGame/static/media/barn3.59e50007.jpg"
+  },
+  {
+    "revision": "8c8824dcafb7441973b9fa26a0e06bd2",
+    "url": "/CowtasticGuessingGame/index.html"
+  }
+];
